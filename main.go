fdbb028406fc8052dbbff9fb296ab2e308ba1bab
gabewitmer <gabenwitmer@gmail.com>
2015-11-17 14:02:39 -0500

    worked on main.go on webmaster

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/cagnosolutions/dbdb"
 	"github.com/cagnosolutions/web"
@@ -16,8 +18,9 @@ var db = dbdb.NewDataStore()
 
 func main() {
 
+	db.AddStore("images")
 	mux.AddRoutes(home, gallery, about, contact, services, listings, floorPlans, login, logout, msg)
-	mux.AddSecureRoutes(ADMIN, webmaster, allListings, uploadImage)
+	mux.AddSecureRoutes(ADMIN, webmaster, allListings, uploadImage, webmasterImage, webmasterDeleteImage)
 	http.ListenAndServe(":8080", mux)
 
 }
@@ -66,6 +69,7 @@ var logout = web.Route{"GET", "/logout", func(w http.ResponseWriter, r *http.Req
 }}
 
 var webmaster = web.Route{"GET", "/webmaster", func(w http.ResponseWriter, r *http.Request) {
+
 	images := db.GetAll("images")
 	tmpl.Render(w, r, "webmaster.tmpl", web.Model{
 		"images": images,
@@ -73,28 +77,28 @@ var webmaster = web.Route{"GET", "/webmaster", func(w http.ResponseWriter, r *ht
 }}
 
 var uploadImage = web.Route{"POST", "/upload-image", func(w http.ResponseWriter, r *http.Request) {
-	path := "static/img/upload"
+	path := "static/img/upload/"
 	if err := os.MkdirAll(path, 0755); err != nil {
-		fmt.Printf("")
+		fmt.Printf("uploadImage >> MkdirAll: %v\n", err)
 		web.SetErrorRedirect(w, r, "/webmaster", "Error uploading file")
 		return
 	}
 	r.ParseMultipartForm(32 << 20) // 32 MB
-	file, handler, err := r.FormFile("logo")
+	file, handler, err := r.FormFile("picture")
 	if err != nil || len(handler.Header["Content-Type"]) < 1 {
-		fmt.Println(err)
+		fmt.Printf("uploadImage >> Header len < 1: %v\n", err)
 		web.SetErrorRedirect(w, r, "/webmaster", "Error uploading file")
 		return
 	}
 	defer file.Close()
 	if handler.Header["Content-Type"][0] != "image/png" && handler.Header["Content-Type"][0] != "image/jpeg" {
-		fmt.Println(err)
+		fmt.Printf("uploadImage >> Header != png || jpeg: %v\n", err)
 		web.SetErrorRedirect(w, r, "/webmaster", "Error uploading file")
 		return
 	}
 	f, err := os.OpenFile(path+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("uploadImage >> OpenFile: %v\n", err)
 		web.SetErrorRedirect(w, r, "/webmaster", "Error uploading file")
 		return
 	}
@@ -106,15 +110,39 @@ var uploadImage = web.Route{"POST", "/upload-image", func(w http.ResponseWriter,
 		"src":         handler.Filename,
 	}
 	db.Add("images", doc)
-	web.SetSuccessRedirect(w, r, "/webmaster", "Successfully Uploaded Image")
+	web.SetSuccessRedirect(w, r, "/webmaster", "Successfully uploaded image")
 	return
 
 }}
 
-var webmasteImage = web.Route{"GET", "/webmaster/:id", func(w http.ResponseWriter, r *http.Request) {
-	tmpl.Render(w, r, "webmaster.tmpl", web.Model{})
+var webmasterImage = web.Route{"GET", "/webmaster/:id", func(w http.ResponseWriter, r *http.Request) {
+	tmpl.Render(w, r, "webmaster.tmpl", web.Model{
+		"images": db.GetAll("images"),
+		"image":  db.Get("images", getId(r.FormValue(":id"))),
+	})
+}}
+
+var webmasterDeleteImage = web.Route{"POST", "/webmaster/:id", func(w http.ResponseWriter, r *http.Request) {
+	img := db.Get("images", getId(r.FormValue(":id")))
+	if img == nil {
+		log.Printf("webmasterDeleteImage >> db.Get: image could not be found in database\n")
+		web.SetErrorRedirect(w, r, "/webmaster", "Error deleting image.")
+		return
+	}
+	if err := os.Remove("static/img/uplaod/" + img.Data["src"].(string)); err != nil {
+		log.Printf("webmasterDeleteImage >> os.Remove: %v\n", err)
+		web.SetErrorRedirect(w, r, "/webmaster", "Error deleting image.")
+		return
+	}
+	db.Del("images", img.Id)
+	web.SetSuccessRedirect(w, r, "/webmaster", "Successfully deleted image")
 }}
 
 var allListings = web.Route{"GET", "/all-listings", func(w http.ResponseWriter, r *http.Request) {
 	tmpl.Render(w, r, "all-listings.tmpl", web.Model{})
 }}
+
+func getId(sid string) uint64 {
+	id, _ := strconv.ParseUint(sid, 10, 64)
+	return id
+}
