981c8af7fd991fda0e3d020d171539420fbbb3c2
Greg Pechiro <gregpechiro@yahoo.com>
2016-03-16 12:55:56 -0400

    updated launch.sh

--- launch.sh
+++ launch.sh
@@ -4,8 +4,6 @@
 # this should only be run on the server where the project lives right before
 # redeployment. It will remove all files that are contained in the new .tar
 
-rm -rf static/ templates/ *.go lhb-go lhb-go.tar
-
 #!/bin/bash
 
 # a script to launch a go project after a tar has been transfered to the server
@@ -22,7 +20,7 @@ DIR=`echo ${PWD##*/}`
 echo "Stopping ${DIR} with supervisor..."
 sudo supervisorctl stop ${DIR}
 echo "Removing old files..."
-rm -rf static/ templates/ *.go ${DIR} ${DIR}.tar
+sudo rm -rf static/ templates/ *.go ${DIR} ${DIR}.tar
 echo "Moving new tar to current location..."
 sudo mv ~/${DIR}.tar .
 echo "Extracting contents of new tar..."
